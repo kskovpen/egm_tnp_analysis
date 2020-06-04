83b1ba111b34fdbf250cd58605dfc8f92a497e4c
tomc <tomc@cern.ch>
2020-06-04 17:28:33 +0200

    Setup script working outside of lxplus

--- etc/scripts/setup.sh
+++ etc/scripts/setup.sh
@@ -1,7 +1,16 @@
 #!bin/bash 
 
-## add python lib
-. /opt/rh/python27/enable
-export LD_LIBRARY_PATH=/opt/rh/python27/root/usr/lib64/:$LD_LIBRARY_PATH
+# Setup for lxplus
+if [[ $(hostname -s) = lxp* ]]; then
+  . /opt/rh/python27/enable
+  export LD_LIBRARY_PATH=/opt/rh/python27/root/usr/lib64/:$LD_LIBRARY_PATH
+  export PYTHONPATH=.:$PYTHONPATH
 
-export PYTHONPATH=.:$PYTHONPATH
+# Setup for somewhere else (just get some CMSSW release which works, slow but it gets the job done)
+else
+  scram p CMSSW CMSSW_10_2_22
+  cd CMSSW_10_2_22/src
+  eval `scram runtime -sh`
+  cd ../../
+  rm -rf CMSSW_10_2_22
+fi
